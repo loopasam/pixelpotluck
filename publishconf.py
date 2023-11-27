f37cfb9885d370f477ea3531573305bacb805ef0
Samuel Croset <samuel.croset@gmail.com>
2023-11-27 17:49:31 +0100

    testing publishin to gandi

--- publishconf.py
+++ publishconf.py
@@ -7,7 +7,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = 'https://loopasam.github.io/pixelpotluck'
+SITEURL = 'http://pixelpotluck.com'
 RELATIVE_URLS = False
 
 OUTPUT_PATH = 'docs/'
